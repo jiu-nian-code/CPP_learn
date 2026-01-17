4ba305d5130c84d4c2708f83e301e84fe006920b
jiu-nian-code <2136614575@qq.com>
2026-01-17 19:01:53 +0800

    1_17

--- 2026/1_16/1_16/test.cpp
+++ 2026/1_16/1_16/test.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<mutex>
 
 //class CopyBan
 //{
@@ -8,21 +9,133 @@
 //	CopyBan& operator=(const CopyBan& self) = delete;
 //};
 
-class CopyBan
+//class CopyBan
+//{
+//public:
+//	CopyBan() {}
+//private:
+//	CopyBan(const CopyBan& self);
+//	CopyBan& operator=(const CopyBan& self);
+//};
+
+//class HeapOnly
+//{
+//public:
+//	static HeapOnly* create_HeapOnly() { return new HeapOnly; }
+//private:
+//	HeapOnly() {}
+//	HeapOnly(const HeapOnly& self) = delete;
+//	HeapOnly& operator=(const HeapOnly& self) = delete;
+//};
+
+class HeapOnly
 {
 public:
-	CopyBan() {}
+	void delete_HeapOnly() { delete this; }
 private:
-	CopyBan(const CopyBan& self);
-	CopyBan& operator=(const CopyBan& self);
+	~HeapOnly() {}
 };
 
+class StackOnly
+{
+public:
+	static StackOnly create_StackOnly() { return StackOnly(); }
+private:
+	void* operator new(size_t size) = delete;
+	void operator delete(void* p) = delete;
+	StackOnly() {};
+};
 
+class NonInherit
+{
+public:
+	static NonInherit create_NonInherit()
+	{
+		return NonInherit();
+	}
+private:
+	NonInherit() {}
+};
+
+class A : public NonInherit
+{
+
+};
+
+//class Singleton
+//{
+//public:
+//	static Singleton* GetInstance() { return &_sl; }
+//private:
+//	Singleton() {}
+//	Singleton(const Singleton& self) = delete;
+//	Singleton& operator=(const Singleton& self) = delete;
+//	static Singleton _sl;
+//};
+
+class Singleton
+{
+public:
+	static Singleton* GetInstance()
+	{
+		if (!_sl)
+		{
+			std::unique_lock<std::mutex> lock(_mt);
+			if (!_sl) _sl = new Singleton;
+		}
+		return _sl;
+	}
+
+	void DelInstance()
+	{
+		std::cout << "DelInstance()" << std::endl;
+		if (_sl)
+		{
+			std::unique_lock<std::mutex> lock(_mt);
+			if (_sl)
+			{
+				delete _sl;
+				_sl = nullptr;
+			}
+		}
+	}
+
+	class gc
+	{
+	public:
+		~gc() { _sl->DelInstance(); }
+	};
+
+	static gc _gc;
+private:
+	Singleton() {}
+	Singleton(const Singleton& self) = delete;
+	Singleton& operator=(const Singleton& self) = delete;
+	static Singleton* _sl;
+	static std::mutex _mt;
+};
+
+Singleton* Singleton::_sl = nullptr;
+Singleton::gc Singleton::_gc;
+std::mutex Singleton::_mt;
+
+class B
+{
+public:
+	~B() { std::cout << "~B()" << std::endl; }
+};
 
 int main()
 {
-	CopyBan cb1;
+	//CopyBan cb1;
 	//CopyBan cb2 = cb1;
 	//CopyBan cb3(cb1);
+	//HeapOnly* ho = new HeapOnly;
+	//ho->delete_HeapOnly();
+	//StackOnly so1 = StackOnly::create_StackOnly();
+	//StackOnly* so1 = new StackOnly;
+	//StackOnly* so2 = new StackOnly(so1);
+	//A a;
+	Singleton* sl = Singleton::GetInstance();
 	return 0;
 }
