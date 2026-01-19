e51d3847aafcd2f60ecfdc112c909df3b3e68608
jiu-nian-code <2136614575@qq.com>
2026-01-19 21:20:28 +0800

    1_19

--- 2026/1_18/1_18/test.cpp
+++ 2026/1_18/1_18/test.cpp
@@ -1,7 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS 1
 #include<iostream>
 #include<vector>
 #include<string>
 #include<ios>
+#include<fstream>
+#include<filesystem>
+#include<sstream>
 
 class B
 {
@@ -20,19 +24,34 @@ public:
 	}
 };
 
+struct Date
+{
+	int _year;
+	int _month;
+	int _day;
+	void PRINT()
+	{
+		std::cout << _year << "-" << _month << "-" << _day << std::endl;
+	}
+};
+
 int main()
 {
-	//A a;
-	////int b = a;
-	//int b = static_cast<int>(a);
-	//std::cout << b << std::endl;
-
-	//std::string str;
-	//while (std::cin >> str)
-	//{
-	//	std::cout << str << std::endl;
-	//}
-
-
+	std::ostringstream oss;
+	std::istringstream iss("hello world 2");
+	oss << "hello world " << 1;
+	std::cout << oss.str() << std::endl;
+	std::string str1, str2;
+	int a;
+	std::cout << iss.str() << std::endl;
+	iss >> str1 >> str2 >> a;
+	std::cout << str1 << " " <<  str2 << " " << a << std::endl;
+	std::cout << iss.str() << std::endl;
+	//´òÓ¡½á¹û:
+	//hello world 1
+	//hello world 2
+	//hello world 2
+	//hello world 2
+	//
 	return 0;
 }
